233cbd7d19fcc7fc1948d29e96c70732ea8208e0
Sambath Prak <sambathprak789@gmail.com>
2024-07-01 13:28:24 +0700

    feature: created form component

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,21 +1,65 @@
+import Button from "../components/button.js";
+import Input from "../components/input.js";
+import Textarea from "../components/textarea.js";
 import { createEntry } from "./crud.js";
-const form = document.createElement('form');
-form.innerHTML = `
-<input type="text">
-<input type="text">
-<input type="date">
-<button type="submit">add</button>`;
-form.addEventListener('submit', ()=> {
-  console.log('submit');
-  createEntry();
-});
-document.querySelector('body').append(form);
+export default class Form extends HTMLElement {
+  constructor(){
+    super();
 
-[...document.querySelectorAll("input")].forEach((elem)=> {
-  if (elem.getAttribute('type') == 'date'){
-    elem.valueAsDate = new Date();
+    const addBtn = new Button('+', 'button', 'show-form btn-primary');
+    const container = document.createElement('div');
+    container.setAttribute('class', 'container');
+    const wrapper = document.createElement('div');
+    wrapper.setAttribute('class', 'grid');
+    const formContainer = document.createElement('div');
+    formContainer.setAttribute('class', 'form-container');
+    const form = document.createElement('form');
+    const inputWrapper = document.createElement('div');
+    inputWrapper.setAttribute('class', 'input-wrapper');
+    const inputTitle = new Input('text', 'title', 'title');
+    const inputContent = new Textarea('content', 'content');
+    const inputDate = new Input('date', 'date', 'date');
+    inputDate.valueAsDate = new Date();
+    const ctaWrapper = document.createElement('div');
+    ctaWrapper.setAttribute('class', 'cta-wrapper');
+    const subBtn = new Button('Create', 'button', 'btn-primary');
+
+
+    addBtn.addEventListener('click', ()=> {
+      formContainer.classList.toggle('visible');
+      if(addBtn.textContent == '+'){
+        addBtn.textContent = '−';
+      }else{
+        addBtn.textContent = '+';
+      }
+    })
+
+    subBtn.addEventListener('click', ()=> {
+      this.formHandler(form, inputTitle.value, inputContent.value, inputDate.value);
+    });
+
+    inputWrapper.append(inputTitle, inputContent, inputDate);
+    ctaWrapper.append(subBtn);
+    form.append(inputWrapper, ctaWrapper);
+    formContainer.append(form);
+    wrapper.append(addBtn, formContainer);
+    container.append(wrapper);
+    this.append(container);
+
   }
-});
+
+  formHandler(form, title, content, date){
+    if(title && content && date){
+      createEntry(title, content, date);
+      // reload browser to update view
+      location.reload(true);
+    }else{
+      alert('Please enter all fields!');
+    };
+
+  }
+}
+window.customElements.define('input-form', Form);
 
 
 
